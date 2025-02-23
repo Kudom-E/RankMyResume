4734b3c19488d3453e595147162b0c2cae330e98
Kudom <emmanuelkudomagyemang@gmail.com>
2025-02-23 04:19:52 -0500

    test get job to see value error

--- src/get_job_text.py
+++ src/get_job_text.py
@@ -4,7 +4,6 @@ import bs4
 
 def get_job_text(job_html):
     if not job_html:
-        print("No job description found.")
         raise ValueError("No job description found.")
 
     with open(job_html, "r", encoding="utf-8") as job_descript:
